d876c88fca06464a36c39c86854cf1e2a6500994
vita133 <vitandrukhiv@gmail.com>
2023-03-20 09:21:56 +0200

    add CI/CD

--- build.gradle.kts
+++ build.gradle.kts
@@ -11,15 +11,19 @@ repositories {
 }
 
 dependencies {
-    testImplementation(kotlin("test"))
-}
-
-tasks.test {
-    useJUnitPlatform()
+    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+    implementation("org.junit.jupiter:junit-jupiter:5.8.1")
+    implementation("junit:junit:4.13.1")
+    testImplementation("org.junit.jupiter:junit-jupiter-api:5.8.1")
+    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.8.1")
 }
 
 kotlin {
     jvmToolchain(8)
+}
+
+tasks.test {
+    useJUnitPlatform()
 }
 
 application {
--- src/test/kotlin/MainTest.kt
+++ src/test/kotlin/MainTest.kt
@@ -1,5 +1,4 @@
-package main.kotlin
-
+import main.kotlin.DoublyLinkedList
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.assertThrows
